bd4196ce4516514b9f1b2760dc0894f1bef304b8
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-05-15 00:30:53 -0500

    Primer commit

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+// En caso de necesitar la implementación del FetchAPI
+// npm i --save-dev isomorphic-fetch
+// o podemos instalar: (para versiones de node ≥18)
+// npm i --save-dev whatwg-fetch
+// import 'isomorphic-fetch'
+// o podemos instalar: (para versiones de node ≥18)
+// import 'whatwg-fetch'
