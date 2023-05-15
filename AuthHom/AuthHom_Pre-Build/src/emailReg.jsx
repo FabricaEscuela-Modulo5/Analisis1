a35310eb7c033b34b56b1940b9df7339391d8951
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-05-14 21:40:02 -0500

    Eliminacion unitarias ficticias

--- AuthHom/AuthHom_Pre-Build/src/emailReg.jsx
+++
@@ -1,6 +0,0 @@
-const emailReg = new RegExp(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/);
-function isEmailRegex(email) {
-  return emailReg.test(email);
-}
-
-module.exports = isEmailRegex;
--- AuthHom/AuthHom_Pre-Build/src/pass.jsx
+++
@@ -1,7 +0,0 @@
-const passReg = new RegExp(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])(?=.{8,})/
-  );
-  const isPassRegex = (password) => {
-    return passReg.test(password);
-  };
-  module.exports = isPassRegex;
